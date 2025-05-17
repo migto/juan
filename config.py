046d810939c28948e761c8829942234afeecd70e
migto <tommy_yang@ustc.edu>
2025-05-17 10:33:57 +0800

    fix bugs

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 
-# 是否开启debug模式
-DEBUG = True
+class Config:
+    # 是否开启debug模式
+    DEBUG = True
 
-# 读取数据库环境变量
-username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'root')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+    # 数据库配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}/flask_demo'.format(
+        os.environ.get("MYSQL_USERNAME", 'root'),
+        os.environ.get("MYSQL_PASSWORD", 'root'),
+        os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
