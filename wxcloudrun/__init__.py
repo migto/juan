355c8854df3dea7052a133264c30305617d1c22c
migto <tommy_yang@ustc.edu>
2025-05-17 21:36:48 +0800

    fix bug

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -1,13 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import pymysql
 from config import Config
-
-# 因MySQLDB不支持Python3，使用pymysql扩展库代替MySQLDB库
-pymysql.install_as_MySQLdb()
-
-# 初始化数据库
-db = SQLAlchemy()
 
 def create_app():
     """
@@ -16,7 +8,6 @@ def create_app():
     """
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
 
     # 注册蓝图
     from wxcloudrun.views import bp
